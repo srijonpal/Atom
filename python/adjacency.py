1d9a0f3ee30980f2ca517366cfe962b2f075e99b
Srijon Pal <srijon09pal@gmail.com>
2022-11-11 23:48:35 +0530

    first python commit

---
+++ python/adjacency.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+def graph(adj):
+    for i in adj:
+        print(i,"-",adj[i])
+n,e = map(int,input().split())
+adj = defaultdict(list)
+for i in range(e):
+	a,b = map(str,input().split())
+	adj[a].append(b)
+	adj[b].append(a)
+
+graph(adj)
